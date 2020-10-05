3e6ff61bf0039d95e78ef89cc321ed1cc206a9fc
gpontesss <pontes.guisilva@gmail.com>
2020-10-05 17:33:40 -0300

    Create tests for LineWalkers

--- pkg/gps/linewalker_test.go
+++ pkg/gps/linewalker_test.go
@@ -3,55 +3,62 @@ package gps
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"testing"
 
 	"github.com/golang/geo/s2"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRestartWalker(t *testing.T) {
-	/*
-		How to describe it visually?
-		It should represent the earth curvature
-				 (90,0)
-			   ____(1)____
-			  /     |     \
-		  (45,-90)  |   (45,90)
-			|   \   |   /   |
-		   (0)   \  |  /   (2)
-		 (0,-90)  \ | /   (0,90)
-	*/
-	path := s2.PolylineFromLatLngs(
-		[]s2.LatLng{
-			s2.LatLngFromDegrees(0, -90),
-			s2.LatLngFromDegrees(90, 0),
-			s2.LatLngFromDegrees(0, -90),
-		},
-	)
+/*
+	How to describe it visually?
+	It should represent the earth curvature
+			 (90,0)
+		   ____(1)____
+		  /     |     \
+	  (45,-90)  |   (45,90)
+		|   \   |   /   |
+	   (0)   \  |  /   (2)
+	 (0,-90)  \ | /   (0,90)
+*/
+var mezzalunaPath = s2.PolylineFromLatLngs(
+	[]s2.LatLng{
+		s2.LatLngFromDegrees(0, -90),
+		s2.LatLngFromDegrees(90, 0),
+		s2.LatLngFromDegrees(0, 90),
+	},
+)
 
-	lw := RestartWalker(path)
-
-	cases := []struct {
+func TestWalkers(t *testing.T) {
+	cases := map[LineWalker][]struct {
 		dist        Distance
-		ll          s2.LatLng
+		endLatLng   s2.LatLng
 		crossedEdge bool
 	}{
-		{DistanceFromMeters(earthRadius * (math.Pi / 4)), s2.LatLngFromDegrees(45, -90), false},
-		{DistanceFromMeters(earthRadius * (math.Pi / 4)), s2.LatLngFromDegrees(90, 0), false},
-		{DistanceFromMeters(4 * (earthRadius * (math.Pi / 4))), s2.LatLngFromDegrees(90, 0), true},
+		RestartWalker(mezzalunaPath): {
+			{DistanceFromMeters(earthRadius * (math.Pi / 4)), s2.LatLngFromDegrees(45, -90), false},
+			{DistanceFromMeters(earthRadius * (math.Pi / 4)), s2.LatLngFromDegrees(90, 0), false},
+			{DistanceFromMeters(earthRadius * math.Pi), s2.LatLngFromDegrees(90, 0), true},
+		},
+		BackForthWalker(mezzalunaPath): {
+			{DistanceFromMeters(earthRadius * (math.Pi / 2)), s2.LatLngFromDegrees(90, 0), false},
+			{DistanceFromMeters(earthRadius * (math.Pi / 4)), s2.LatLngFromDegrees(45, 90), false},
+			{DistanceFromMeters(earthRadius * (math.Pi / 2)), s2.LatLngFromDegrees(45, 90), true},
+		},
 	}
 
-	for _, c := range cases {
-		t.Run(fmt.Sprint(c.dist, c.ll), func(t *testing.T) {
-			ll, ce := lw.Walk(c.dist)
-			assert.True(t, c.ll.ApproxEqual(ll), "Expected: %v/Result: %v", c.ll, ll)
-			assert.Equal(t, c.crossedEdge, ce)
-		})
+	for lw, walks := range cases {
+		for _, walk := range walks {
+			t.Run(
+				fmt.Sprintf("%s/Distance-%.6f/Stop-%v", reflect.TypeOf(lw), walk.dist, walk.endLatLng),
+				func(t *testing.T) {
+					ll, ce := lw.Walk(walk.dist)
+					assert.True(t, walk.endLatLng.ApproxEqual(ll), "Expected: %v/Result: %v", walk.endLatLng, ll)
+					assert.Equal(t, walk.crossedEdge, ce)
+				},
+			)
+		}
 	}
-}
-
-func TestBackAndForthWalker(t *testing.T) {
-	panic("TODO")
 }
 
 func TestMetersToS1Angle(t *testing.T) {
@@ -59,6 +66,7 @@ func TestMetersToS1Angle(t *testing.T) {
 		in     float64
 		result Distance
 	}{
+		{earthRadius, 1},
 		{earthRadius * math.Pi, math.Pi},
 	}
 
